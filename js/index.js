0952fb5dbd584be0b5c97dd89ba6fa3ea73afd42
youngbin <redmangos@naver.com>
2022-05-01 23:35:11 +0900

    gltfloader

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import * as THREE from "/three/build/three.module.js";
 import { OrbitControls } from "/three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
